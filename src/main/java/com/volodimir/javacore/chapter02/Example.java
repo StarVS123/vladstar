307500d0d5cc00609a544aa32edbf84998241398
volodimir <vsstarchenko@gmail.com>
2022-07-16 10:49:46 +0300

    example listing added

---
+++ src/main/java/com/volodimir/javacore/chapter02/Example.java
@@ -0,0 +1,8 @@
+package com.volodimir.javacore.chapter02;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Простая программа на Java.");
+    }
+}
+
