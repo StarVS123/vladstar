9d5391de6e5535394052c38746d0840a953c5304
volodimir <vsstarchenko@gmail.com>
2022-07-16 13:08:55 +0300

    second version

---
+++ src/main/java/com/volodimir/javacore/chapter02/ForTest1.java
@@ -0,0 +1,12 @@
+package com.volodimir.javacore.chapter02;
+
+public class ForTest1 {
+    public static void main(String[] args) {
+        int x;
+        for (x = 0; x<10; x++)
+            System.out.println("Значение x: " + x);
+    }
+}
+
+
+
