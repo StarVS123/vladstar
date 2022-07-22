83992308661baeb7fe38bd91936308edbd2867a7
volodimir <vsstarchenko@gmail.com>
2022-07-22 23:18:31 +0300

    changed

--- src/main/java/com/volodimir/javacore/chapter03/TwoDAgain.java
+++ src/main/java/com/volodimir/javacore/chapter03/TwoDAgain.java
@@ -17,7 +17,6 @@ public class TwoDAgain {
             }
         }
 
-
         for (i = 0; i < 4; i ++) {
             for (j = 0; j < i + 1; j++) {
                 System.out.print(twoD [i][j] + " ");
