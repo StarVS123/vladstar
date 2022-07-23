bbc273a417a0dbbdaf0683fbbad791d99e538461
volodimir <vsstarchenko@gmail.com>
2022-07-23 11:16:23 +0300

    this is not difficult too

---
+++ src/main/java/com/volodimir/javacore/chapter05/NoBody.java
@@ -0,0 +1,15 @@
+package com.volodimir.javacore.chapter05;
+
+public class NoBody {
+    public static void main(String[] args) {
+        int i, j;
+
+        i = 100;
+        j = 200;
+
+        // рассчитать среднее значение i и j
+        while (++i < --j); // у этого цикла отсутствует тело
+        System.out.println("Среднее значение равно " + i);
+
+    }
+}
