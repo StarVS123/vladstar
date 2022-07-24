77cd47f74e988d23184569a19703722ed1674e24
volodimir <vsstarchenko@gmail.com>
2022-07-24 09:44:51 +0300

    figured out

---
+++ src/main/java/com/volodimir/javacore/chapter05/BreakLoop3.java
@@ -0,0 +1,15 @@
+package com.volodimir.javacore.chapter05;
+
+public class BreakLoop3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Проход " + i + ": ");
+            for (int j = 0; j < 100; j++) {
+                if (j == 10) break; //выход из цикла, значение переменной j = 10
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Циклы завершены.");
+    }
+}
