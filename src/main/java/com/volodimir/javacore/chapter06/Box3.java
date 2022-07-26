ce65cc2995ff0326ba81e007c30bb3e34ded42c3
volodimir <vsstarchenko@gmail.com>
2022-07-26 10:32:09 +0300

    good

--- src/main/java/com/volodimir/javacore/chapter06/Box3.java
+++ src/main/java/com/volodimir/javacore/chapter06/Box3.java
@@ -2,38 +2,39 @@ package com.volodimir.javacore.chapter06;
 
 class Box3 {
     double width;
-    double  hight;
+    double hight;
     double depth;
 
     // вывести объем паралеллепипеда
     void volume() {
         System.out.print("Объем равен ");
-        System.out.println(width*hight*depth);
+        System.out.println(width * hight * depth);
     }
 
 }
+
 class BoxDemo3 {
-public static void main(String[]args){
-        Box mybox1 = new Box();
-        Box mybox2 = new Box();
+    public static void main(String[] args) {
+        Box3 mybox1 = new Box3();
+        Box3 mybox2 = new Box3();
 
         // присвоить значение переменным экземпляра mybox1
-    mybox1.width = 10;
-    mybox1.haight = 20;
-    mybox1.depht = 15;
+        mybox1.width = 10;
+        mybox1.hight = 20;
+        mybox1.depth = 15;
 
     /* присвоить другие значения переменным экземпляра mebox2
     экземпляра mybox2 */
-    mybox2.width = 3;
-    mybox2.haight = 6;
-    mybox2.depht = 9;
+        mybox2.width = 3;
+        mybox2.hight= 6;
+        mybox2.depth = 9;
 
-    // вывести объем первого параллелепипеда
-    mybox1.volume();
+        // вывести объем первого параллелепипеда
+        mybox1.volume();
 
-    // вывести объем второго параллелепипеда
-    mybox2.volume();
+        // вывести объем второго параллелепипеда
+        mybox2.volume();
+    }
 }
-        }
 
 
