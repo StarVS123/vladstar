00babc06cc9d7eb5ddda31bc32559bb1958fe30d
volodimir <vsstarchenko@gmail.com>
2022-08-03 08:25:02 +0300

    ready

---
+++ src/main/java/com/volodimir/javacore/chapter08/TestBalance.java
@@ -0,0 +1,12 @@
+package com.volodimir.javacore.chapter08;
+
+import com.volodimir.javacore.chapter08.MyPack.*;
+
+public class TestBalance {
+    public static void main(String[] args) {
+        /* класс Balance объявлен как public, поэтому им можно воспользоваться и вызвать его конструктор */
+        Balance test = new Balance("J. J. Jaspers", 99.88);
+
+        test.show();
+    }
+}
