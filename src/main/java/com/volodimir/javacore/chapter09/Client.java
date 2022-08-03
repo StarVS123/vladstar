c50585edb644ac7e079a3217628842ad8aa0a31e
volodimir <vsstarchenko@gmail.com>
2022-08-03 09:03:28 +0300

    rewritten

---
+++ src/main/java/com/volodimir/javacore/chapter09/Client.java
@@ -0,0 +1,12 @@
+package com.volodimir.javacore.chapter09;
+
+class Client implements Callback {
+    // реализовать интерфейс Callback
+    public void callback (int p) {
+
+        System.out.println("Метод Callback вызываемый со значением " + p);
+    }
+    void nonIfaceMeth() {
+        System.out.println("В классах, реализующих интерфейсы, " + "могут определяться и другие члены.");
+    }
+}
