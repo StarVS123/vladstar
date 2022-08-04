d9c12e48bfc7e752d95dbf555504e780c97e3800
volodimir <vsstarchenko@gmail.com>
2022-08-04 23:01:45 +0300

    generally understandable

--- src/main/java/com/volodimir/javacore/chapter09/IntStack.java
+++ src/main/java/com/volodimir/javacore/chapter09/IntStack.java
@@ -2,5 +2,5 @@ package com.volodimir.javacore.chapter09;
 
 public interface IntStack {
     void push (int item); //сохранить элемент в стеке
-    int pop();              //извлечь элемент из стека
+    int pop();            //извлечь элемент из стека
 }
