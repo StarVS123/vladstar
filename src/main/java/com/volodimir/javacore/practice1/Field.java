adabecd907cf22c23988fffebdf70e5b0212f80d
volodimir <vsstarchenko@gmail.com>
2022-08-11 16:14:37 +0300

    added new comments

--- src/main/java/com/volodimir/javacore/practice1/Field.java
+++ src/main/java/com/volodimir/javacore/practice1/Field.java
@@ -6,19 +6,20 @@ public class Field {
     // объявляем массив из 9 ячеек
     private final String[] values = new String[9];
 
-    //метод сеттер, подставляет значение в выбранную ячейку (номер ячейки от 0 до 8, значение "Х" или "0")
+    // метод сеттер, подставляет значение в выбранную ячейку (номер ячейки от 0 до 8, значение "Х" или "0")
     public void setField(int index, String value) {
-        index --;
+        index--;
         this.values[index] = value;
     }
 
+    // метод геттер, возвращает доступ к массиву values
     public String[] getValues() {
         return values;
     }
 
     //метод, выводящий массив в консоль
     public void showField() {
-        for (int i = 0; i < 9; i ++) {
+        for (int i = 0; i < 9; i++) {
             System.out.print(values[i] + " ");
             int y = i + 1;
             if (y % 3 == 0) {
--- src/main/java/com/volodimir/javacore/practice1/GameLogic.java
+++ src/main/java/com/volodimir/javacore/practice1/GameLogic.java
@@ -1,18 +1,21 @@
 package com.volodimir.javacore.practice1;
 
-import java.security.cert.X509Certificate;
 import java.util.Scanner;
 
 public class GameLogic {
 
+    // дает доступ к объектам класса Field
     private final Field field = new Field();
 
+    // импортирует сканео, позволяющий использовать результат ввода игрока
     private final Scanner scanner = new Scanner(System.in);
 
+    // переменные
     private final String X = "X";
 
     private final String O = "O";
 
+    // метод, позволяющий начать игру и дающий возможность игрокам вводить свой выбор поочередно
     public void start() {
         boolean isXTurn = true;
         for (int i = 0; i < 9; i++) {
@@ -32,6 +35,7 @@ public class GameLogic {
         }
     }
 
+    // метод, позволяющий первому игроку сделать корректный ввод
     private void makesFirstUserMove() {
         boolean correctInput = false;
         do {
@@ -49,6 +53,7 @@ public class GameLogic {
         while (correctInput != true);
     }
 
+    // метод, позволяющий второму игроку сделать корректный ввод
     private void makeSecondUserMove() {
         boolean correctInput = false;
         do {
@@ -66,14 +71,13 @@ public class GameLogic {
         while (correctInput != true);
     }
 
+    // метод, определяющий победителя
     private String getWinner() {
 
         String[] values = field.getValues();
-        if ((values[0] != null && values[1] != null && values[2] != null) && values[0].equals(values[1]) && values[1].equals(values[2])
-        {
+        if ((values[0] != null && values[1] != null && values[2] != null) && values[0].equals(values[1]) && values[1].equals(values[2])) {
             return values[0];
-        } else
-        if ((values[3] != null && values[4] != null && values[5] != null) && values[3].equals(values[4]) && values[4].equals(values[5])) {
+        } else if ((values[3] != null && values[4] != null && values[5] != null) && values[3].equals(values[4]) && values[4].equals(values[5])) {
             return values[3];
         } else if ((values[6] != null && values[7] != null && values[8] != null) && values[6].equals(values[7]) && values[7].equals(values[8])) {
             return values[6];
@@ -91,6 +95,7 @@ public class GameLogic {
         return null;
     }
 
+    // метод, определяющий, заполнены ли все ячейки игрового поля
     private boolean isFieldFull() {
 
         boolean result = true;
