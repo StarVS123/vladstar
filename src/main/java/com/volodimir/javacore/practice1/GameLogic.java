3617dac729dd22a7ed4ceb8a5b43b308815f9edc
volodimir <vsstarchenko@gmail.com>
2022-08-11 11:34:32 +0300

    added new comments

--- src/main/java/com/volodimir/javacore/practice1/GameLogic.java
+++ src/main/java/com/volodimir/javacore/practice1/GameLogic.java
@@ -1,6 +1,10 @@
 package com.volodimir.javacore.practice1;
 
+import java.util.Objects;
 import java.util.Scanner;
+
+import static com.volodimir.javacore.practice.Field.*;
+import static com.volodimir.javacore.practice.Field.activePlayer;
 
 public class GameLogic {
 
@@ -8,15 +12,32 @@ public class GameLogic {
 
     private final Scanner scanner = new Scanner(System.in);
 
-    public void start(){
-    }
-    private void makesFirstUserMove(){
+    public void start() {
     }
 
-    private void makeSecondUserMove(){
+    private void makesFirstUserMove() {
+        String value = "X";
+        boolean correctInput = false;
+        do {
+            int choice = scanner.nextInt();
+            if (choice >= 0 && choice <= 8) {
+            } else {
+                System.out.println("Неправильный выбор. Повторите ввод.");
+            }
+        }
+        while (correctInput != true);
     }
 
-    private String getWinner(){
+    private void makeSecondUserMove() {
+        String value = "0";
+        boolean correctInput = false;
+        do {
+        }
+        while (correctInput != true);
+    }
+
+    private String getWinner() {
+        return null;
     }
 }
 
