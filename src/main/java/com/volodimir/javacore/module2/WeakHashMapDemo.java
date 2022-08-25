a86ff50a2721be0a4a6872aae119269bacb2d450
volodimir <vsstarchenko@gmail.com>
2022-08-25 12:02:48 +0300

    parse

---
+++ src/main/java/com/volodimir/javacore/module2/WeakHashMapDemo.java
@@ -0,0 +1,39 @@
+package com.volodimir.javacore.module2;
+
+import  java.util.Map;
+import java.util.IllegalFormatWidthException;
+import java.util.WeakHashMap;
+
+public class WeakHashMapDemo {
+
+    private static Map map;
+
+    public static void main(String[] args) {
+        map = new WeakHashMap<>();
+        map.put(new Integer (1), "Proselyte");
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                while (map.containsKey(1)){
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                    System.out.println("Thread is waiting...");
+                    System.gc();
+                }
+            }
+        };
+        Thread thread = new Thread();
+        thread.start();
+        System.out.println("Application is waiting");
+        try {
+            thread.join();
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+
+        }
+    }
+}
